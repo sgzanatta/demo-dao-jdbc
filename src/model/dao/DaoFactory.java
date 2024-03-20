c0c077d0ef22f86032cd937734eb31d697a47c66
Sérgio Zanatta <sgzanatta92@gmail.com>
2024-03-20 15:51:24 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -4,7 +4,12 @@ import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
+
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 }
